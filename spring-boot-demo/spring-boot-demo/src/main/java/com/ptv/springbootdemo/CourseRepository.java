a5dc1d5ea68f438a758885aa15ec56d0f7657411
Puneet Vashisht <puneetvashisht@outlook.com>
2021-09-08 14:37:06 +0530

    test

--- spring-boot-demo/spring-boot-demo/src/main/java/com/ptv/springbootdemo/CourseRepository.java
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/ptv/springbootdemo/CourseRepository.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CourseRepository extends JpaRepository<Course, Integer>{
+
 	
+
 	List<Course> findByTitle(String title);
 	List<Course> findByUser(User user);
 
---
+++ spring-boot-demo/spring-boot-demo/src/main/java/com/ptv/springbootdemo/CourseService.java
@@ -0,0 +1,8 @@
+package com.ptv.springbootdemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+}
